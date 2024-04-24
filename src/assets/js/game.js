d98748f7ccd304a9db215269ce9a069717895370
b21phiro <philip.rosenqvist@outlook.com>
2024-04-24 11:00:12 +0200

    Allows canvas to resize based on parent. Also changes during resize

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -7,7 +7,23 @@ class Game {
      * @param canvas { HTMLCanvasElement }
      */
     constructor(canvas) {
+
         this._canvas = canvas;
+        this._setCanvasSizeToParent();
+
+        // Resizes the canvas element to the parent
+        // when the browser changes size.
+        window.onresize = (ev) => {
+            this._setCanvasSizeToParent();
+        };
+
+    }
+
+    _setCanvasSizeToParent() {
+        const aspectRatio = 16 / 9;
+        const parentElement = this._canvas.parentElement;
+        this._canvas.width = parentElement.offsetWidth;
+        this._canvas.height = parentElement.offsetWidth / aspectRatio;
     }
 
 }
